175d1c2fd23ba090c38251cf6b5b6003b90d4c8c
Tony Vincent <tonyvince7@gmail.com>
2020-08-08 21:19:29 +0200

    Use factorubot for fixture replacement

--- config/application.rb
+++ config/application.rb
@@ -31,10 +31,10 @@ module Syncforce
     # the framework and any gems in your application.
     config.generators do |g|
       g.test_framework :rspec,
-                       fixtures: false,
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
   end
 end
